6f268617e87c693328637b1d65d40b1c5e0c461b
HaFiid98 <hafiditsme@gmail.com>
2023-03-18 13:31:19 -0700

    l

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,22 +4,23 @@
 /**
  * main - The entry point of my program
  *
- * Description: The program prints the alphabet in lowercase.
- *followed by a new line.
+ * Description: The program prints the alphabet in lowercase
+ * execpt 'q' and 'e' followed by a new line.
  *
  * Return: Return O if there is no error in main program
  */
 int main(void)
 {
-	int n;
+	int n = 0;
 	char alphabet[28] = "abcdefghijklmnopqrstuvwxyz";
 
-	while (alphabet[n] != '\0'){
+	while (alphabet[n] != '\0')
+	{
 		if (alphabet[n] != 'e' && alphabet[n] != 'q')
 		putchar(alphabet[n]);
 		n++;
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
