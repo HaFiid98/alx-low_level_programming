99db6469b4c3d92629036ea1c7e45a6600dcd88d
HaFiid98 <hafiditsme@gmail.com>
2023-03-21 15:20:19 -0700

    l

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/* -main prints _putchar, followed by a new line
+ * Description : prints _putchar, followed by a new line
+ * return : 0
+ */
+int main(void)
+{
+	int i;
+	char pt[8]="-putchar";
+	for (i=0 ; i <=8 ; i++ )
+	{
+		if( pt[i] != '\0' )
+		putchar(pt[i]);
+	}
+	putchar('\n');
+	return(0);
+}
