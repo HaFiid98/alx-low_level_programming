61af24bb70a6484a4ea1f3e6b0af8e70fbe0712d
HaFiid98 <hafiditsme@gmail.com>
2023-03-21 13:15:38 -0700

    l

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,21 +1,27 @@
 #include<stdio.h>
-int main (void) 
+
+
+/*This algorithm print the combination of 3 numbers without 
+ * printing dublicates of the same number or even with piveotation
+ * like 01 and 10 */
+
+int main(void) 
 { 
 	int first;
 	int second;
 	int third;
 
-	for (first=48;first<=57;first++)
+	for (first = 48; first <= 57; first++)
 	{ 
-		for(second=first+1;second<=57;second++)
+		for (second=first+1;second<=57;second++)
 		{
-			for(third=second+1;third<=57;third++)
+			for (third = second+1; third <= 57; third++)
 			{
 				putchar(first);
 				putchar(second);
 				putchar(third);
 
-				if (!(first==55 && second==56 && third==57))
+				if (!(first == 55 && second == 56 && third == 57))
 				{
 					putchar(',');
 				        putchar(' ');
@@ -24,9 +30,6 @@ int main (void)
 		}
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 	
-}	
-
-				
-				
+}			
