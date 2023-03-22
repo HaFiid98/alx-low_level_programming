37e8d683a19af1bf3c18cabfa6c2660b392417d3
HaFiid98 <hafiditsme@gmail.com>
2023-03-22 06:06:13 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,10 +3,8 @@
  * print-sign - A function that print the sign of a number
  *
  * @n: is the number whom check their sign
- *
- * Return: 1 if @n is greater than zero 
- * Return: 0 and prints 0 if n is zero
- * Return: -1 and prints - if n is less than zero
+ * Description : function that prints the sign of a number
+ * Return: 1 if @n is greater than zero; 0 and prints 0 if n is zero; -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
@@ -25,4 +23,4 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-}	
+}
