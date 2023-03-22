8b0d5eb964112c7c34410a1d9e3f5944c8f52f31
HaFiid98 <hafiditsme@gmail.com>
2023-03-22 12:40:59 -0700

    l

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,31 +9,31 @@
  */
 void jack_bauer(void)
 {
-        int minute;
-        int minutes;
-        int hour;
+	int minute;
+	int minutes;
+	int hour;
 	int hours;
 	int compt = 57;
-        for (hour = 48 ; hour < 51 ; hour++)
-        {
-                if (hour == 50)
-		{ 
+	for (hour = 48 ; hour < 51 ; hour++)
+	{
+		if (hour == 50)
+		{
 			compt = 51;
-		}	
-                        for (hours = 48 ; hours <= compt ; hours++)
-                        {
-                        for (minute = 48 ; minute < 54 ; minute++)
-                                {
-                                for (minutes = 48 ; minutes <= 57 ; minutes++)
-                                        {
-                                        _putchar(hour);
-                                        _putchar(hours);
-                                        _putchar(':');
-                                        _putchar(minute);
-                                        _putchar(minutes);
-                                        _putchar('\n');
-                                        }
+		}
+		for (hours = 48 ; hours <= compt ; hours++)
+				{
+					for (minute = 48 ; minute < 54 ; minute++)
+					{
+						for (minutes = 48 ; minutes <= 57 ; minutes++)
+						{
+							_putchar(hour);
+							_putchar(hours);
+							_putchar(':');
+						       	_putchar(minute);
+							_putchar(minutes);
+							_putchar('\n');
+						}
+					}
 				}
-			}
 	}
 }
