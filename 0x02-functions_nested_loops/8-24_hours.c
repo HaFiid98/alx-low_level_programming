7d0d1c611eaf6444450380a7dfe6551745ff5302
HaFiid98 <hafiditsme@gmail.com>
2023-03-22 12:30:44 -0700

    l

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,53 +1,39 @@
 #include"main.h"
-/* jack_bauer - is a function that print every minute of the day
- *  * Description: The program prints all minute of the day
- *  Return:return 0
- *  */
+/**
+ * jack_bauer - A function who prints every minute of the day
+ * from 00:00 to 23:59
+ *
+ * Description - Does not receive any parameters
+ *
+ * Return: Does not return anything
+ */
 void jack_bauer(void)
 {
-	int minute;
-	int minutes;
-	int hour;
+        int minute;
+        int minutes;
+        int hour;
 	int hours;
-
-	for (hour = 48 ; hour < 51 ; hour++)
-	{
-		if ( hour != 50 )
-		{
-			for (hours = 48 ; hours <= 57 ; hours++)
-		{
-			for (minute = 48 ; minute < 54 ; minute++)
-			{
-				for (minutes = 48 ; minutes <=57 ; minutes++)
-				{
-					_putchar(hour);
-					_putchar(hours);
-					_putchar(':');
-					_putchar(minute);
-					_putchar(minutes);
-					_putchar('\n');
-				}
-			}
-		}
-		}
-
-		else
-		{
-			         for (hours = 48 ; hours < 52 ; hours++)
-                {
-                        for (minute = 48 ; minute < 54 ; minute++)
+	int compt = 57;
+        for (hour = 48 ; hour < 51 ; hour++)
+        {
+                if (hour == 50)
+		{ 
+			compt = 51;
+		}	
+                        for (hours = 48 ; hours <= compt ; hours++)
                         {
-                                for (minutes = 48 ; minutes <=57 ; minutes++)
+                        for (minute = 48 ; minute < 54 ; minute++)
                                 {
+                                for (minutes = 48 ; minutes <= 57 ; minutes++)
+                                        {
                                         _putchar(hour);
                                         _putchar(hours);
                                         _putchar(':');
                                         _putchar(minute);
                                         _putchar(minutes);
                                         _putchar('\n');
-                                }
-                        }
-                }
-		}
+                                        }
+				}
+			}
 	}
 }
