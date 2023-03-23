1c4d163e524b17b78d9aa1dddec80aef2b605478
HaFiid98 <hafiditsme@gmail.com>
2023-03-23 16:18:14 -0700

    l

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include"main.h"
+/**
+ * main - The entry point of my program
+ *
+ * Return: Return O if there is no error in main program
+ */
+int postitive_or_negative(int n)
+{
+        if (n > 0)
+                printf("%d is positive\n", n);
+        else if (n < 0)
+                printf("%d is negative\n", n);
+        else
+                printf("0 is zero\n");
+
+        return (0);
+}                                        
