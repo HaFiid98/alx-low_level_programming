90129281a17d7f0a15b18d7dd03b5afbd9ce6acb
HaFiid98 <hafiditsme@gmail.com>
2023-03-24 16:25:55 -0700

    ll

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,40 @@
+#include"main.h"
+/**
+ * more_numbers - A function who print the numbers, from 0 to 14 X10
+ *
+ * Description - Does not receive any parameters
+ *
+ * Return: Does not return anything
+ */
+void more_numbers(void)
+{
+	int i;
+	int c;
+	int b,x,t;
+
+for (t = 48 ; t <= 57 ; t++)
+{
+	b = 48;
+	x = 0;
+	c = 57;
+	while (x < 2)
+	{
+	for (i = 48 ;i <= c ; i++)
+	{
+		if (b == 49)
+		{ 
+			_putchar(49);
+		}
+		if (i == 57)
+		{
+			c = 52;
+			b = 49;
+		}
+		_putchar(i);
+		
+	}
+		x++;
+}
+		_putchar('\n');
+}
+}
