458a28a794c41e0cc07a3eee398bb79dc9a31c11
HaFiid98 <hafiditsme@gmail.com>
2023-03-27 18:18:34 -0700

    l

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,12 @@
 /**
  * reset_to_98 - finction take a pointer a param and update the pointto 98
  *
- * @*n : is the parameter
+ * @n : is the parameter
  *
  * return : nothing
  *
  */
 void reset_to_98(int *n)
 {
-	*n = 98; 
+	*n = 98;
 }
