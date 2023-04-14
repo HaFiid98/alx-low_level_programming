28e11718286a2665be78741e0f1c7178b2bf0049
HaFiid98 <hafiditsme@gmail.com>
2023-04-14 09:58:49 -0700

    l

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -5,13 +5,16 @@
 /**
  * free_grid -free 2 dimensialn grid
  * @height: height of the grid
- *
+ * @grid: it the grid lmao
  * Return: Nothing.
  */
 void free_grid(int **grid, int height)
 {
 	int m;
-
+	if (grid == NULL)
+	{
+		free(grid);
+	}
 	free(grid);
 	for (m = 0; m <= height; m++)
 	{
