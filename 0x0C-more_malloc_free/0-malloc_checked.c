a99d78d58907b96a4813393f8f1e40e1d8cfe26e
HaFiid98 <hafiditsme@gmail.com>
2023-04-15 00:28:00 -0700

    l

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,11 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *p = malloc(sizeof(unsigned int) * b);
+	void *p = malloc(b);
 
 	if (p == NULL)
 	{
 		exit(98);
 	}
 	return (p);
-	free (p);
 }
