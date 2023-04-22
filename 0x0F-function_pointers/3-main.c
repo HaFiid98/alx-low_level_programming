0a48a98250118d8ae008d44eb08bebe52e409322
HaFiid98 <hafiditsme@gmail.com>
2023-04-22 04:31:27 -0700

    l

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,32 +1,34 @@
-#include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
-
+#include"3-calc.h"
 /**
- * main - ...
- * @argc: ...
- * @argv: ...
- *
- * Return: ...
+ * main - main
+ * @argc: argulent count
+ * @argv: argument content
+ * Return: idk
  */
 int main(int argc, char *argv[])
-{
-	int (*oprt)(int, int);
-
-	if (argc != 4)
 	{
-		printf("Error\n");
-		exit(98);
+		int a, b, c;
+		int (*function)(int, int);
+
+		if (argc != 4)
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+		function = get_op_func(argv[2]);
+		if (!function)
+		{
+			printf("Error\n");
+			exit(99);
+		}
+		if (((*argv[2] == '/') || (*argv[2] == '%')) && (b == 0))
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		c = function(a, b);
+		printf("%d\n", c);
+		return (0);
 	}
-
-	oprt = get_op_func(argv[2]);
-
-	if (!oprt)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
-	return (0);
-}
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,59 +1,51 @@
-#include "3-calc.h"
-
+#include"3-calc.h"
 /**
- * op_add - calculates the sum of two integers
- * @a: first integer
- * @b: second integer
- *
- * Return: sum of a and b
+ * op_add - add twi integers
+ * @a: int 1
+ * @b: iny 2
+ * Return: the sum of the two
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- * op_sub - calculates the difference of two integers
- * @a: first integer
- * @b: second integer
- *
- * Return: difference of a and b
+ * op_sub- syb twi integers
+ * @a: int 1
+ * @b: iny 2
+ * Return: the sub of the two
  */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
- * op_mul - calculates the product of two integers
- * @a: first integer
- * @b: second integer
- *
- * Return: product of a and b
+ * op_mul- mul twi integers
+ * @a: int 1
+ * @b: iny 2
+ * Return: the mul of the two
  */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
- * op_div - calculates the division of two integers
- * @a: first integer
- * @b: second integer
- *
- * Return: result of the division of a and b
+ * op_div- div twi integers
+ * @a: int 1
+ * @b: int 2
+ * Return: the div of the two
  */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-
 /**
- * op_mod - calculates the remainder of the division of two integers
- * @a: first integer
- * @b: second integer
- *
- * Return: remainder of a divided b
+ * op_mod - mpdulo twi integers
+ * @a: int 1
+ * @b: int 2
+ * Return: the mod of the two
  */
 int op_mod(int a, int b)
 {
