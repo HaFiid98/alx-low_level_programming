1a923f79cf4603d4c1e61a936271f18f0d76c717
HaFiid98 <hafiditsme@gmail.com>
2023-04-22 11:41:08 -0700

    l

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 						if (separator)
 						{
 							printf("%d", x);
-							if (!(i == n - 2))
+							if (!(i == n - 1))
 								printf(", ");
 						}
 				}
