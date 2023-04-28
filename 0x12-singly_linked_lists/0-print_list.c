4fbfbc5306ad9ea2e60c19c45741c024e71aaf26
HaFiid98 <hafiditsme@gmail.com>
2023-04-28 12:22:55 -0700

    l

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include"lists.h"
+#include <stdio.h>
+/**
+ * print_list - print list on linked list
+ * @h: is the list
+ * Return: return the nulber of nods
+ */
+size_t print_list(const list_t *h)
+{
+	size_t b = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u]%s\n", h->len, h->str);
+		h = h->next;
+		b++;
+	}
+	return(b);
+}
