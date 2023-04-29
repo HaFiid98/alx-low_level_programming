22475ac2ab9332a1d9e6fc7bb82ba4db729096fb
HaFiid98 <hafiditsme@gmail.com>
2023-04-28 17:15:57 -0700

    l

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,6 +8,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t b = 0;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,5 +18,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		b++;
 	}
-	return(b);
+	return (b);
 }
