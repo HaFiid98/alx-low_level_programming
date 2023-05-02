6df5839a2b476acaa4627502f0c16cd2a6171626
HaFiid98 <hafiditsme@gmail.com>
2023-05-01 21:43:42 -0700

    l

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include"lists.h"
+/**
+ * pop_listint - delete the head of the node
+ * @head: input
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *r;
+	int data;
+
+	if (*head == NULL)
+		return (0);
+	data = (*head)->n;
+	r = (*head)->next;
+	free(*head);
+	*head = r;
+	return (data);
+}
