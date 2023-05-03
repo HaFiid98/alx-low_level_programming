4bb1ec7e5c66f41f8079ad4e3b7617d261fdc4de
HaFiid98 <hafiditsme@gmail.com>
2023-05-02 17:10:18 -0700

    l

--- 0x08-recursion/p.c
+++ 0x08-recursion/p.c
@@ -1,11 +1,35 @@
 #include"main.h"
-void _puts_recursion(char *s)
+#include<stdio.h>
+
+void rev(char*s, char *b);
+void rev(char *s, char *b)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		return;
+
 	}
-	_putchar(*s);
-	_puts_recursion(s+1);
+	b = s;
+		 rev(s + 1,b);
+
+  printf("%s", b);
 }
+int is_palindrome(char *s)
+{
+	char *b ="" ;
+	rev (s + 1, b + 1);
+	if ( *s == '\0')
+	{
+		return (1);
+	}
+	if (s != b)
+	{
+		return (0);
+	}
+	if (s == b)
+	{
+		return (1);
+	}
+	return (is_palindrome(s + 1));
+}
+
