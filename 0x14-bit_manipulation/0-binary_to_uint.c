0736754562cb5a8a16f3ebf9d96e260c0467b79b
HaFiid98 <hafiditsme@gmail.com>
2023-05-16 19:33:48 -0700

    l

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,27 +6,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int ar[12] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048};
-unsigned int r = 0;
+unsigned int k = 0;
 int i;
-int coun = 0;
 
 if (b == NULL)
 	return (0);
-while (b[coun] != '\0')
+for (i = 0; b[i]; i++)
 {
-	coun++;
-}
-for (i = coun - 1; i >= 0 &&  b ; i--)
-{
-	if ((b[i] != '0') && (b[i] != '1'))
-	{
+	if (b[i] != '1' && b[i] != '0')
 		return (0);
-	}
-	if (b[i] == '1')
-	{
-		r = r + ar[coun - i];
-	}
-	}
-return (r / 2);
+	k = 2 * k + (b[i] - '0');
+}
+return (k);
 }
